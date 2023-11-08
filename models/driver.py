0242d5c4ef0658ca32b02110ab538f76942b9601
Z3nd <jonhrip9@gmail.com>
2023-11-08 21:38:29 +0300

    -- change table Driver

--- models/driver.py
+++ models/driver.py
@@ -1,14 +1,12 @@
 from . import Base
 from sqlalchemy import (
-    Integer,
     Column,
     String,
-    DateTime)
+    Date)
 
 
 class Driver(Base):
-    driver_id = Column(Integer, primary_key=True, nullable=False)
-    driver_name = Column(String(30), nullable=False)
-    driver_license_number = Column(String(30), nullable=False, unique=True)
-    driver_address = Column(String(30), nullable=False)
-    expiration_date = Column(DateTime, nullable=False)
+    driver_name = Column(String(100), nullable=False)
+    driver_license_number = Column(String(100), nullable=False, unique=True)
+    driver_address = Column(String(100), nullable=False)
+    expiration_date = Column(Date, nullable=False)
