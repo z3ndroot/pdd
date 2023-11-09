95f875d0e83e88382d9a5c56494ce59d842f6e3e
Z3nd <jonhrip9@gmail.com>
2023-11-09 14:45:00 +0300

    -- add str method and relationship

--- models/driver.py
+++ models/driver.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
     Column,
     String,
     Date)
+from sqlalchemy.orm import relationship
 
 
 class Driver(Base):
@@ -10,3 +11,16 @@ class Driver(Base):
     driver_license_number = Column(String(100), nullable=False, unique=True)
     driver_address = Column(String(100), nullable=False)
     expiration_date = Column(Date, nullable=False)
+
+    cars = relationship('Car', back_populates='driver')
+
+    def __str__(self):
+        return (
+            f"{self.__class__.__name__}("
+            f"id={self.id}, "
+            f"driver_name={self.driver_name}, "
+            f"driver_license_number={self.driver_license_number}, "
+            f"driver_address={self.driver_address}, "
+            f"expiration_date={self.expiration_date} "
+            f")"
+        )
